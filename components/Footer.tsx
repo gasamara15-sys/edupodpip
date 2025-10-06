2873d0ddb6e559a7809385682972c9a5db4ed768
gasamara15-sys <gasamara15@gmail.com>
2025-10-06 15:31:13 +0900

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,1 +1,8 @@
+export default function Footer() {
+  return (
+    <footer style={{borderTop:"1px solid #e5e7eb", marginTop:24}}>
+      <div className="container meta">© {new Date().getFullYear()} EduPod</div>
+    </footer>
+  );
+}
 
