a65e2450bd20150097d32d9b021dc48c10570ee5
gasamara15-sys <gasamara15@gmail.com>
2025-10-06 15:36:26 +0900

    Update QuizCard.tsx

--- app/learn/components/QuizCard.tsx
+++ app/learn/components/QuizCard.tsx
@@ -1,1 +1,11 @@
+"use client";
+export default function QuizCard() {
+  return (
+    <div className="card">
+      <h3 style={{marginTop:0}}>오늘의 3문항 (샘플)</h3>
+      <p className="meta">추후 서버 액션 연결</p>
+      <button className="btn">시작</button>
+    </div>
+  );
+}
 
