33cf851404d4746086936411018af13877c322dc
gasamara15-sys <gasamara15@gmail.com>
2025-10-06 15:31:30 +0900

    Update TTSPlayer.tsx

--- components/TTSPlayer.tsx
+++ components/TTSPlayer.tsx
@@ -1,1 +1,9 @@
+"use client";
+export default function TTSPlayer({ text }: { text: string }) {
+  const speak = () => {
+    const u = new SpeechSynthesisUtterance(text);
+    u.lang = "en-US"; window.speechSynthesis.speak(u);
+  };
+  return <button className="btn" onClick={speak}>TTS 재생</button>;
+}
 
