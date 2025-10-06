7524cc4fcb5c2cd4a996a3b1a03164ed5987b50b
gasamara15-sys <gasamara15@gmail.com>
2025-10-06 15:30:56 +0900

    Update NavBar.tsx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,1 +1,14 @@
+import Link from "next/link";
+
+export default function NavBar() {
+  return (
+    <nav className="nav">
+      <Link href="/" className="brand">EduPod</Link>
+      <Link href="/news">뉴스</Link>
+      <Link href="/keywords">키워드</Link>
+      <div style={{marginLeft:"auto"}} />
+      <Link href="/account">마이페이지</Link>
+    </nav>
+  );
+}
 
