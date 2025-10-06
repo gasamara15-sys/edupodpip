7de76eb32df82642916f81797261cfc2fa90bfd6
gasamara15-sys <gasamara15@gmail.com>
2025-10-06 15:36:59 +0900

    Update page.tsx

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -1,1 +1,11 @@
+import QuizCard from "./components/QuizCard";
+
+export default async function LearnPage() {
+  return (
+    <div className="grid">
+      <h1>학습</h1>
+      <QuizCard />
+    </div>
+  );
+}
 
