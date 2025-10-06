99fbdfceee67647aed19a1736fa34a32dad3cfc2
gasamara15-sys <gasamara15@gmail.com>
2025-10-06 15:28:21 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: { serverActions: { allowedOrigins: ['*'] } }
+  reactStrictMode: true
 };
 export default nextConfig;
-
